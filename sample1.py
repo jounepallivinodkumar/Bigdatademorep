a9b5ab61c01f5587b034ede883e0f44462679fb1
jounepallivinodkumar <117571413+jounepallivinodkumar@users.noreply.github.com>
2022-12-14 13:27:25 +0530

    Create sample1.py

---
+++ sample1.py
@@ -0,0 +1,18 @@
+int_var = 10
+float_var = 10.56
+string_var = "Hello"
+bool_var = True
+
+print("hello world!!")
+
+print("Type of int_var = ",int_var)
+#print("Type of float_var = ",float_var)
+print("Type of float_var = ",float_var)
+print("Type of string_var = ",string_var,'result done')
+print("Type of bool_var =",bool_var,"-done")
+
+print("data type of int = ",type(int_var))
+
+number_str = "123"
+number_str = int(number_str)+50
+print(number_str)
